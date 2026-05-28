2132295eef84d96797349661582e91e740aaae07
techn <kushagra.official.73@gmail.com>
2026-05-28 20:26:38 +0530

    main

---
+++ src/Q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("Enter a number upto which you want the sum : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i < n+1; i++) {
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}

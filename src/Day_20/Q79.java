acff4944a25c1b7160d229e52a3a07bdd3ad8ab5
techn <kushagra.official.73@gmail.com>
2026-06-21 00:04:17 +0530

    main

---
+++ src/Day_20/Q79.java
@@ -0,0 +1,27 @@
+package Day_20;
+
+import java.util.Scanner;
+
+public class Q79 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows and columns: ");
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int[][] matrix = new int[rows][cols];
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Row wise Sum:");
+        for (int i = 0; i < rows; i++) {
+            int sum = 0;
+            for (int j = 0; j < cols; j++) {
+                sum += matrix[i][j];
+            }
+            System.out.println("Sum of Row " + (i + 1) + " = " + sum);
+        }
+    }
+}
---
+++ src/Day_20/Q80.java
@@ -0,0 +1,26 @@
+package Day_20;
+
+import java.util.Scanner;
+public class Q80 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows and columns: ");
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int[][] matrix = new int[rows][cols];
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Column-wise Sum:");
+        for (int j = 0; j < cols; j++) {
+            int sum = 0;
+            for (int i = 0; i < rows; i++) {
+                sum += matrix[i][j];
+            }
+            System.out.println("Sum of Column " + (j + 1) + " = " + sum);
+        }
+    }
+}
